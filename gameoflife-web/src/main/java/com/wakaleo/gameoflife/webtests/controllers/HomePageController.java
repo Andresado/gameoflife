9218d2bf071f50d58c472160fb486dc023f1a178
Andres Guevara <e_afcastillo@latam.stefanini.com>
2020-08-10 12:18:28 -0500

    actualizacion

---
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -0,0 +1,25 @@
+package com.wakaleo.gameoflife.webtests.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * This class rocks.
+ *
+ * @author johnsmart
+ */
+@Controller
+public class HomePageController {
+	// FIXME: do something nicer for the home page
+    @RequestMapping("/")
+    public ModelAndView index() {
+    	System.out.println("Redirecting to home...");
+        return new ModelAndView("home");
+    }
+
+    @RequestMapping("/home")
+    public ModelAndView home() {
+        return new ModelAndView("home");
+    }
+}
